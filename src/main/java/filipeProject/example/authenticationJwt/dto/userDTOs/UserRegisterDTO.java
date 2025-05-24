ae9283177634c8c84f2cf5d9da66a7f6852e8c61
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-05-24 11:53:58 -0300

    add Validation dependency

--- src/main/java/filipeProject/example/authenticationJwt/dto/userDTOs/UserRegisterDTO.java
+++ src/main/java/filipeProject/example/authenticationJwt/dto/userDTOs/UserRegisterDTO.java
@@ -1,6 +1,9 @@
 package filipeProject.example.authenticationJwt.dto.userDTOs;
 
 import filipeProject.example.authenticationJwt.entities.User;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
